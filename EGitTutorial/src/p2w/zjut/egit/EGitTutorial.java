c838704f297cd56da05f2c322484f115332fc590
pww8424 <pww8424@gmail.com>
2012-12-13 13:16:16 +0800

    Add Local In 2012-12-13

--- EGitTutorial/src/p2w/zjut/egit/EGitTutorial.java
+++ EGitTutorial/src/p2w/zjut/egit/EGitTutorial.java
@@ -4,5 +4,6 @@ public class EGitTutorial {
 	public static void main(String[] args) {
 		System.out.println("Git is fun!");
 		System.out.println("Git is cool!");
+		System.out.println("2012-12-13_In Local!");
 	}
 }
